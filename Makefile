b53a7c67ebaecdd5981eded3db9a5f00bf370b55
Yuichiro NAITO <naito.yuichiro@gmail.com>
2021-04-02 00:23:45 +0900

    move vm operation functions to vm.c.

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 SUBDIR=	plugins
 PROG=	bhyved
-SRCS=	bhyved.c conf.c tap.c parser.c
+SRCS=	bhyved.c conf.c tap.c parser.c vm.c
 CFLAGS+=-Wall
 
 .include <bsd.prog.mk>
---
+++ vm.h
@@ -0,0 +1,17 @@
+#ifndef _VM_H_
+#define _VM_H_
+
+#include "vars.h"
+
+int write_mapfile(struct vm *vm);
+pid_t grub_load(struct vm *vm);
+pid_t bhyve_load(struct vm *vm);
+int remove_taps(struct vm *vm);
+int activate_taps(struct vm *vm);
+int assign_taps(struct vm *vm);
+int exec_bhyve(struct vm *vm);
+int destroy_vm(struct vm *vm);
+int start_vm(struct vm *vm);
+void cleanup_vm(struct vm *vm);
+
+#endif
