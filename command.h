364927d96abad845d0cde4fc833e53b579ce10de
Yuichiro Naito <naito.yuichiro@gmail.com>
2021-09-10 12:45:23 +0900

    add include "vars.h"

--- command.h
+++ command.h
@@ -1,6 +1,8 @@
 #ifndef _COMMAND_H
 #define _COMMAND_H
 
+#include "vars.h"
+
 int connect_to_server(const struct global_conf *gc);
 int create_command_server(const struct global_conf *gc);
 int accept_command_socket(int s0);
