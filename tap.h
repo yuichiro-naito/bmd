36962bfca9a36d6f26b8295f8529468a693d4c90
Yuichiro NAITO <naito.yuichiro@gmail.com>
2023-07-15 23:06:36 +0900

    Delete tap.h.

--- tap.h
+++
@@ -1,9 +0,0 @@
-#ifndef _TAP_H_
-#define _TAP_H_
-
-int activate_tap(int s, const char *name);
-int add_to_bridge(int s, const char *bridge, const char *tap);
-int create_tap(int s, char **name);
-int destroy_tap(int s, const char *name);
-int set_tap_description(int s, const char *tap, char *desc);
-#endif
--- vm.h
+++ vm.h
@@ -3,10 +3,18 @@
 
 struct vm;
 
+/* Implemented in vm.c */
 int remove_taps(struct vm *vm);
 int activate_taps(struct vm *vm);
 int assign_taps(struct vm *vm);
 int write_err_log(int fd, struct vm *vm);
 int write_mapfile(struct vm_conf *conf, char **mapfile);
+
+/* Implemented in tap.c */
+int add_to_bridge(int s, const char *bridge, const char *tap);
+int activate_tap(int s, const char *name);
+int create_tap(int s, char **name);
+int destroy_tap(int s, const char *name);
+int set_tap_description(int s, const char *tap, char *desc);
 
 #endif
