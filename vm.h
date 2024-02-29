52bfb30f65ab4d2ca39cd79320a7e5da578fbb0b
Hiroki Sato <hrs@allbsd.org>
2024-02-29 21:15:53 +0900

    vm: Add prototype of split_args()

--- vm.h
+++ vm.h
@@ -11,6 +11,7 @@ int activate_taps(struct vm *);
 int assign_taps(struct vm *);
 int write_err_log(int , struct vm *);
 int write_mapfile(struct vm_conf *, char **);
+char **split_args(char *);
 
 /* Implemented in tap.c */
 int add_to_bridge(int , const char *, const char *);
