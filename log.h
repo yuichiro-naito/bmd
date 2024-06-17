098ab10276f9e48406d17273ae47baec9dd05a3e
Yuichiro NAITO <naito.yuichiro@gmail.com>
2024-06-17 23:55:00 +0900

    Allow to use [info|warn|err] logging functions for plugins.

--- log.h
+++ log.h
@@ -6,6 +6,7 @@
 #define LOG_OPEN_PERROR() openlog("bmd", LOG_PID | LOG_PERROR, LOG_DAEMON)
 #define LOG_CLOSE() closelog()
 
+#define LOGGER(pri, msg, ...) syslog(pri, msg, __VA_ARGS__)
 #define ERR(msg, ...) syslog(LOG_ERR, msg, __VA_ARGS__)
 #define WARN(msg, ...) syslog(LOG_WARNING, msg, __VA_ARGS__)
 #define INFO(msg, ...) syslog(LOG_INFO, msg, __VA_ARGS__)
