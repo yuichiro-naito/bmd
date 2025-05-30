29a661e1be10c81e4ae8df578568ade2dc01ce89
Yuichiro NAITO <naito.yuichiro@gmail.com>
2025-05-30 14:47:25 +0900

    Remove unused header files.

--- bmd.c
+++ bmd.c
@@ -25,7 +25,6 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
-#include <sys/param.h>
 #include <sys/dirent.h>
 #include <sys/event.h>
 #include <sys/procctl.h>
--- conf.c
+++ conf.c
@@ -25,8 +25,6 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
-#include <sys/param.h>
-
 #include <ctype.h>
 #include <errno.h>
 #include <stdlib.h>
